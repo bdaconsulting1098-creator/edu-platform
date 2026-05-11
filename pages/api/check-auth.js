04749f15c1136cb4ac1e63e5ea8a5cb9c7eb746c
bdaconsulting1098-creator <bdaconsulting1098@gmail.com>
2026-05-11 00:33:45 -0400

    clean: 删除冲突的 pages 和 middleware.js

--- pages/api/check-auth.js
+++
@@ -1,9 +0,0 @@
-export default function handler(req, res) {
-  const authCookie = req.cookies.auth
-  
-  if (authCookie === 'logged-in') {
-    return res.status(200).json({ authenticated: true })
-  }
-  
-  return res.status(401).json({ authenticated: false })
-}
